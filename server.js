05a6550617883b4a7fb97e9e12c70cfef93f41ea
Jody Lorist <jodylorist@hotmail.com>
2021-02-08 02:59:48 +0100

    created index file + server file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http')
+const fs = require('fs')
 
 const hostname = 'localhost'
 const port = 8080
+const index = fs.readFileSync('index.html')
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World!\n')
+    res.setHeader('Content-Type', 'text/html')
+    res.end(index)
 })
 
 server.listen(port, hostname, () => {
