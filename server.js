b615d17730620054e15f78d952bc4dc417a0ad45
Jody Lorist <jodylorist@hotmail.com>
2021-02-09 13:49:58 +0100

    changed to index.ejs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const hostname = 'localhost'
 const port = 8080
-const index = fs.readFileSync('index.html')
+const index = fs.readFileSync('index.ejs')
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
