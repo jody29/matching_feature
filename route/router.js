28e2d2c9ada5aa0fcd38bd5a02bf9c9add6f0b3e
Jody Lorist <jodylorist@hotmail.com>
2021-02-11 03:08:28 +0100

    [update] classes in nav dynamic, h1 is dynamic

--- route/router.js
+++ route/router.js
@@ -4,20 +4,40 @@ const path = require('path')
 
 router.get('/', function (req, res) {
     res.render('pages/index', {
-        title: 'Home'
+        title: 'Home',
+        currentHome: 'current',
+        currentProfile: 'none',
+        currentPreference: 'none'
+        
     })
 })
 
 router.get('/profile', function (req, res) {
     res.render('pages/profile', {
-        title: 'Profile'
+        title: 'Profile',
+        currentProfile: 'current',
+        currentHome: 'none',
+        currentPreference: 'none'
+    })
+})
+
+router.get('/preference', function (req, res) {
+    res.render('pages/preference', {
+        title: 'Preferences',
+        currentPreference: 'current',
+        currentProfile: 'none',
+        currentHome: 'none'
+        
     })
 })
 
 router.get('*', function (req, res) {
     res.status(404).render('pages/404', {
         url: req.url,
-        title: 'Error 404'
+        title: 'Error 404',
+        currentPreference: 'none',
+        currentProfile: 'none',
+        currentHome: 'none'
     })
 })
 
