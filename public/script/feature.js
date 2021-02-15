a3d674e793ff86970cc561f677387681e0faa88a
Jody Lorist <jodylorist@hotmail.com>
2021-02-15 01:34:53 +0100

    added js to index.ejs, preference menu pops up with js

---
+++ public/script/feature.js
@@ -0,0 +1,9 @@
+let preferenceMenu = document.querySelector('main div:first-of-type');
+let preferenceButton = document.querySelector('main section:first-of-type button');
+let preferenceImage = document.querySelector('main section:first-of-type button img');
+
+const showPreferences = () => {
+    preferenceMenu.classList.toggle('showPreferences');
+};
+
+preferenceButton.addEventListener('click', showPreferences);
