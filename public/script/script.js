40c94e72acdb35ee5c6be9783af33cf3bff0181b
Jody Lorist <jodylorist@hotmail.com>
2021-03-11 15:56:43 +0100

    [update] changes made due to linter

--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,11 @@
-let preferenceMenu = document.querySelector('#matchPage main div:first-of-type');
-let preferenceButton = document.querySelector('#matchPage main section:first-of-type button');
-let preferenceImg = document.querySelector('#matchPage main section:first-of-type button img')
+let preferenceMenu = document.querySelector('#matchPage main div:first-of-type')
+let preferenceButton = document.querySelector(
+    '#matchPage main section:first-of-type button'
+)
+let preferenceImg = document.querySelector(
+    '#matchPage main section:first-of-type button img'
+)
 
-preferenceButton.addEventListener('click', function() {
-    preferenceMenu.classList.toggle('showPreferences');
-
-});
-
-
-
-
+preferenceButton.addEventListener('click', function () {
+    preferenceMenu.classList.toggle('showPreferences')
+})
